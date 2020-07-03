b222a6029420ebd2eb3f6a70b41d1b74a72e8f5d
root <root@vultr.guest>
2020-07-03 17:19:39 +0700

    fix bugs

--- resources/views/front/layouts/header.blade.php
+++ resources/views/front/layouts/header.blade.php
@@ -7,22 +7,22 @@
     <!-- CSRF Token -->
     @yield('meta')
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">{{-- 
     <link href="https://fonts.googleapis.com/css?family=Lora:400,400i,700,700i&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('assets\front\css\open-iconic-bootstrap.min.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface&display=swap" rel="stylesheet"> --}}{{-- 
+    <link rel="stylesheet" href="{{asset('assets\front\css\open-iconic-bootstrap.min.css')}}"> --}}
     <link rel="stylesheet" href="{{asset('assets\front\css\animate.css')}}">
-    
+    {{-- 
     <link rel="stylesheet" href="{{asset('assets\front\css\owl.carousel.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\owl.theme.default.min.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\magnific-popup.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\aos.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\ionicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('assets\front\css\owl.theme.default.min.css')}}"> --}}{{-- 
+    <link rel="stylesheet" href="{{asset('assets\front\css\magnific-popup.css')}}"> --}}
+    <link rel="stylesheet" href="{{asset('assets\front\css\aos.css')}}">{{-- 
+    <link rel="stylesheet" href="{{asset('assets\front\css\ionicons.min.css')}}"> --}}{{-- 
     <link rel="stylesheet" href="{{asset('assets\front\css\bootstrap-datepicker.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\jquery.timepicker.css')}}">
+    <link rel="stylesheet" href="{{asset('assets\front\css\jquery.timepicker.css')}}"> --}}{{-- 
     
-    <link rel="stylesheet" href="{{asset('assets\front\css\flaticon.css')}}">
-    <link rel="stylesheet" href="{{asset('assets\front\css\icomoon.css')}}">
+    <link rel="stylesheet" href="{{asset('assets\front\css\flaticon.css')}}"> --}}{{-- 
+    <link rel="stylesheet" href="{{asset('assets\front\css\icomoon.css')}}"> --}}
     <link rel="stylesheet" href="{{asset('assets\front\css\style.css')}}">
     <link rel="stylesheet" href="{{asset('assets\css\prism.css')}}">
   </head>
